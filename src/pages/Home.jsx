b349427c4ac34a81b7f80cb05512da3cd9007e8e
Szuu1 <suanchristopherb220100815@gmail.com>
2026-08-19 10:29:31 +0800

    Navigate: adding useNavigate state inside homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,33 @@
+import { useNavigate } from 'react-router-dom';
+
 function Home() {
+
+    const navigate = useNavigate();
+
+    const goToAbout = () => {
+        navigate('/About');
+    }
+
+    const goToContact = () => {
+        navigate('/Contact');
+    }
+
+    const goToInterests = () => {
+        navigate('/Interests');
+    }       
   return (
     <div>
-      <h1>Welcome to the Home Page</h1>
-      <p>This is the main landing page of our application.</p>
+      <h1>Welcome to My Portfolio</h1>
+      <p>Hi! I'm Christopher B. Suan</p>
+
+      <div>
+        <button onClick={goToAbout}>About Me</button>
+        <button onClick={goToContact}>Contact Me</button>
+        <button onClick={goToInterests}>My Interests</button>
+      </div>
     </div>
+
   );
 }
+
+export default Home;
