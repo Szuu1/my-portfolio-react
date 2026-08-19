91b10f9db82f72676c9c6d69a3f2ff6d377b7fd3
Szuu1 <suanchristopherb220100815@gmail.com>
2026-08-19 09:52:23 +0800

    Page: Adding Contact page

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,8 @@
+function Contact() {
+  return (
+    <div>
+      <h1>Contact Us</h1>
+      <p>If you have any questions, please reach out to us at suanchristopherb220100815@gmail.com.</p>
+    </div>
+  );
+}
