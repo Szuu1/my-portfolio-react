5be8289a5cad4aeec55746fde583fc14891537c1
Szuu1 <suanchristopherb220100815@gmail.com>
2026-08-19 11:27:03 +0800

    State: importing use state and adding function to Home Page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,33 +1,24 @@
-import { useNavigate } from 'react-router-dom';
+import { useState } from "react";
 
-function Home() {
+export default function Home() {
 
-    const navigate = useNavigate();
+    const [showIntro, setShowIntro] = useState(false);
 
-    const goToAbout = () => {
-        navigate('/About');
-    }
-
-    const goToContact = () => {
-        navigate('/Contact');
-    }
-
-    const goToInterests = () => {
-        navigate('/Interests');
-    }       
-  return (
+    return (
     <div>
       <h1>Welcome to My Portfolio</h1>
       <p>Hi! I'm Christopher B. Suan</p>
 
-      <div>
-        <button onClick={goToAbout}>About Me</button>
-        <button onClick={goToContact}>Contact Me</button>
-        <button onClick={goToInterests}>My Interests</button>
-      </div>
-    </div>
+      <button onClick={() => setShowIntro(!showIntro)}>
+        {showIntro ? "Hide Introduction" : "Show Introduction"}
+      </button>
 
+      {showIntro && (
+        <p>
+          I am interested in web development and creating applications
+          using React.
+        </p>
+      )}
+    </div>
   );
 }
-
-export default Home;
