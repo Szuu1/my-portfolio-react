a8d6190e9a935024cafd708f3147d9b2a12b8d84
Szuu1 <suanchristopherb220100815@gmail.com>
2026-08-19 11:39:49 +0800

    State: Adding useState for contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,19 @@
-function Contact() {
+import {useState} from 'react';
+
+export default function Contact() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
   return (
     <div>
-      <h1>Contact Us</h1>
-      <p>If you have any questions, please reach out to us at suanchristopherb220100815@gmail.com.</p>
+      <h1>Contact Me</h1>
+      
+      <input type="text" placeholder="Enter your name" value={name} onChange={(e) => setName(e.target.value)} />
+      <br> </br>
+      <input type="email" placeholder="Enter your email" value={email} onChange={(e) => setEmail(e.target.value)} />
+
+      <p>Name: {name}</p>
+      <p>Email: {email}</p>
     </div>
   );
 }
